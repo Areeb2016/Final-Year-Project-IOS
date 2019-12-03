003b2650231f6d2f8af34a060b23a5907b02e0cf
BitesDigital <BitesDigital@users.noreply.github.com>
2019-12-03 16:41:50 +0500

    Ninth Commit

---
+++ DigitalBitesIOS/DigitalBitesIOS/Category.swift
@@ -0,0 +1,20 @@
+//
+//  Category.swift
+//  DigitalBitesIOS
+//
+//  Created by Digital Bites on 03/12/2019.
+//  Copyright © 2019 Digital Bites. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Category{
+    var image: UIImage
+    var title: String
+    
+    init(image: UIImage, title: String){
+        self.image = image
+        self.title = title
+    }
+}
