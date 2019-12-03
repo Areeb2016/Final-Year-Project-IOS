13fef082de9eeecd89977d6629764acd7c631599
BitesDigital <BitesDigital@users.noreply.github.com>
2019-12-03 21:46:19 +0500

    Tenth Commit

--- Digital Bites/Digital Bites/ViewController.swift
+++ Digital Bites/Digital Bites/ViewController.swift
@@ -22,6 +22,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.navigationItem.hidesBackButton = true
+        
         // Do any additional setup after loading the view.
         DesignPart()
     }
